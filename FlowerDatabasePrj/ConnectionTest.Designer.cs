f0bdd0b4c85799e6f43e0de02096790804f4feaf
AneeMandmets <anee.mandmets@gmail.com>
2023-06-11 11:45:09 +0300

    Add project files.

---
+++ FlowerDatabasePrj/ConnectionTest.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace FlowerDatabasePrj
+{
+    partial class ConnectionTest
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnConnect = new System.Windows.Forms.Button();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblState = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnConnect
+            // 
+            this.btnConnect.Location = new System.Drawing.Point(140, 59);
+            this.btnConnect.Name = "btnConnect";
+            this.btnConnect.Size = new System.Drawing.Size(75, 23);
+            this.btnConnect.TabIndex = 0;
+            this.btnConnect.Text = "Connect";
+            this.btnConnect.UseVisualStyleBackColor = true;
+            this.btnConnect.Click += new System.EventHandler(this.btnConnect_Click);
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(86, 137);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(98, 13);
+            this.lblStatus.TabIndex = 1;
+            this.lblStatus.Text = "Connection status: ";
+            // 
+            // lblState
+            // 
+            this.lblState.AutoSize = true;
+            this.lblState.Location = new System.Drawing.Point(202, 137);
+            this.lblState.Name = "lblState";
+            this.lblState.Size = new System.Drawing.Size(78, 13);
+            this.lblState.TabIndex = 2;
+            this.lblState.Text = "Not connected";
+            // 
+            // ConnectionTest
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 208);
+            this.Controls.Add(this.lblState);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.btnConnect);
+            this.Name = "ConnectionTest";
+            this.Text = "Connection test";
+            this.Load += new System.EventHandler(this.ConnectionTest_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnConnect;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblState;
+    }
+}
+
---
+++ FlowerDatabasePrj/ConnectionTest.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySqlConnector;
+
+namespace FlowerDatabasePrj
+{
+    public partial class ConnectionTest : Form
+    {
+        MySqlConnection conn; // Connection variable
+        string myConnectionString; // String for connection data
+        bool myIsConnected = false; // Boolean value for whether the connection is open or not
+        public ConnectionTest()
+        {
+            InitializeComponent();
+        }
+
+        private void ConnectionTest_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private bool DatabaseConnection()
+        {
+            myConnectionString = "server=localhost;uid=root;pwd=L4ste4i40pet4j4;database=lilled";
+
+            try
+            {
+                conn = new MySqlConnection(myConnectionString);
+                conn.Open();
+            }
+            catch (MySqlException ex) 
+            { 
+                MessageBox.Show(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        private void btnConnect_Click(object sender, EventArgs e)
+        {
+            myIsConnected = DatabaseConnection();
+            if (myIsConnected)  // If the connectios is successful, changes the status text
+            {
+                lblState.Text = "Connected";
+                lblState.ForeColor = Color.Green;
+            }
+            else
+            {
+                lblState.ForeColor = Color.Red;
+            }
+        }
+    }
+}
